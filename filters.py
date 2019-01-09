6bbf6c6819ade06e702e3ff71cf62b0d47fa06e5
Pavol Rusnak <pavol@rusnak.io>
2019-01-09 19:39:08 +0100

    convert codebase from Py2 to Py3; use black for formatting

--- filters.py
+++ filters.py
@@ -1,28 +1,27 @@
 import jinja2
 import flask
 
-blueprint = flask.Blueprint('filters', __name__)
+blueprint = flask.Blueprint("filters", __name__)
+
 
 @jinja2.contextfilter
 @blueprint.app_template_filter()
 def format_thousands_int(context, n):
-    return "{0:,d}".format(long(n))
+    return "{0:,d}".format(int(n))
+
 
 @jinja2.contextfilter
 @blueprint.app_template_filter()
 def format_thousands_float(context, n):
     return "{0:,.2f}".format(n)
 
+
 @jinja2.contextfilter
 @blueprint.app_template_filter()
 def convert_bytes(context, n):
-    suffix = {
-      'TB': 1000 ** 4,
-      'GB': 1000 ** 3,
-      'MB': 1000 ** 2,
-      'KB': 1000,
-    }
-    for k, v in sorted(suffix.items(), key=lambda x: x[1], reverse=True):
-      if n / v >= 1:
-        return '%.2f %s' % (float(n) / v, k)
-    return '%.2f %s' % (n, 'B')
+    # keep the following list sorted
+    suffixes = [("TB", 1000 ** 4), ("GB", 1000 ** 3), ("MB", 1000 ** 2), ("KB", 1000)]
+    for k, v in suffixes:
+        if n / v >= 1:
+            return "%.2f %s" % (float(n) / v, k)
+    return "%.2f %s" % (n, "B")
