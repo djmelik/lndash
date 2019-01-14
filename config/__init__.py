259b82208b4fb14edae70b20b43ec25ece7ce66b
Pavol Rusnak <pavol@rusnak.io>
2019-01-14 21:49:44 +0100

    config: add possibility to override lnd server via env variable

--- config/__init__.py
+++ config/__init__.py
@@ -1,4 +1,7 @@
+import os
+
 macaroon_path = "config/readonly.macaroon"
 cert_path = "config/tls.cert"
-lnd_grpc_server = "127.0.0.1:10009"
 grpc_max_length = 32 * 1024 * 1024
+
+lnd_grpc_server = os.getenv("LNDASH_LND_SERVER", "127.0.0.1:10009")
