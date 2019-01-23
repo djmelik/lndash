e2fdbfd8b19dc9b6adad777aefa31dcf13244ba4
Pavol Rusnak <stick@gk2.sk>
2019-01-23 12:50:18 +0100

    nix: add shell.nix

---
+++ shell.nix
@@ -0,0 +1,9 @@
+with import <nixpkgs> {};
+
+let
+  myPython = python36.withPackages(p: [p.click p.flask p.flask-caching p.googleapis_common_protos p.grpcio p.grpcio-tools p.gunicorn p.itsdangerous p.jinja2 p.markupsafe p.protobuf p.werkzeug ] ++ [ p.black ]);
+in
+  stdenv.mkDerivation {
+    name = "lndash-dev";
+    buildInputs = [ myPython ];
+  }
