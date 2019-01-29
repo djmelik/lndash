38e640a90b88052a20cf55a5b1ab92a7d87fd757
Pavol Rusnak <pavol@rusnak.io>
2019-01-29 02:06:52 +0100

    nix: use python3, not python36

--- shell.nix
+++ shell.nix
@@ -1,7 +1,7 @@
 with import <nixpkgs> {};
 
 let
-  myPython = python36.withPackages(p: [p.click p.flask p.flask-caching p.googleapis_common_protos p.grpcio p.grpcio-tools p.gunicorn p.itsdangerous p.jinja2 p.markupsafe p.protobuf p.werkzeug ] ++ [ p.black ]);
+  myPython = python3.withPackages(p: [p.click p.flask p.flask-caching p.googleapis_common_protos p.grpcio p.grpcio-tools p.gunicorn p.itsdangerous p.jinja2 p.markupsafe p.protobuf p.werkzeug ] ++ [ p.black ]);
 in
   stdenv.mkDerivation {
     name = "lndash-dev";
